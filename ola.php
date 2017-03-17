8adab11fd8f3830ad59efd9017b0aedeabfad4ad
carlosserrao <carlos.j.serrao@gmail.com>
2017-03-17 13:32:59 +0000

    Initial commit

---
+++ ola.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cserrao
+ * Date: 10/03/2017
+ * Time: 19:25
+ */
+
+$a = array(1 => 1, 2, 3, 4, 5, 6, 7);
+
+print_r($a);
+
+foreach ($a as $e) {
+    echo "$e \n";
+}
+
+
+?>
---
+++ phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cserrao
+ * Date: 10/03/2017
+ * Time: 20:11
+ */
+
+phpinfo();
